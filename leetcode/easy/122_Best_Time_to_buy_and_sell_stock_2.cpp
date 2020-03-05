725313173044b4397ece1bec02c35e9a710644d9
swdream <swdream.a@gmail.com>
2020-03-05 23:09:16 +0900

    leetcode easy

---
+++ leetcode/easy/122_Best_Time_to_buy_and_sell_stock_2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size()==1) return 0;
+        int maxProfit = 0;
+        for(int i = 1; i < prices.size(); ++i){
+            if(prices[i] > prices[i-1]){
+                maxProfit += prices[i]-prices[i-1];
+            }
+        }
+        return maxProfit;
+    }
+};
