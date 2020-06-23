f80565583fa034c034731b9476e880f513bbbb81
eunho <eunho.lee@stunitas.com>
2020-06-23 20:24:15 +0900

    interpole search

--- data_structure/interpolSearch.cpp
+++ data_structure/interpolSearch.cpp
@@ -5,15 +5,16 @@ using namespace std;
 
 int interpoleSearch(int arr[], int first, int last, int target){
     if(first > last) return -1;
+    if(arr[first] > target || arr[last] < target) return -1;
     int mid = (double)(target-arr[first])/(arr[last]-arr[first]) * (last - first) + first;
     
     if(arr[mid] < target){
-        return interpoleSearch(arr, mid, last, target);
+        return interpoleSearch(arr, mid+1, last, target);
     }
     else if(arr[mid] == target){
         return mid;
     }
     else{
-        return interpoleSearch(arr, first, mid, target);
+        return interpoleSearch(arr, first, mid-1, target);
     }
 }
